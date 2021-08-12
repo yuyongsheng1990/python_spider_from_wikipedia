018a250bd3235016cfa975121f74c162854078d7
yuyongsheng <yysgzyx@163.com>
2021-08-12 16:48:29 +0800

    编写爬取wikipedia信息的python爬虫

---
+++ spider_clawing.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+# @Project -> File: python_spider_from_wikipedia -> spider_clawing
+# @Time: 2021/8/12 16:43 
+# @Author: Yu Yongsheng
+# @Description:
---
+++ spider_restfulAPI.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+# @Project -> File: python_spider_from_wikipedia -> spider_restfulAPI
+# @Time: 2021/8/12 16:45 
+# @Author: Yu Yongsheng
+# @Description:
